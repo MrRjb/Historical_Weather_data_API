7b030043346eb125954a2ea3a0a5df573fb6196f
= <=>
2023-08-23 21:20:47 +0530

    Added Files

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+import pandas
+
+app = Flask("Website")
+
+stations = pandas.read_csv("data\stations.txt", skiprows=17)
+stations = stations[["STAID","STANAME                                 "]]
+
+@app.route("/")
+def home():
+    return render_template("home.html", data=stations.to_html())
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    filename = "data\TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pandas.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+    
+@app.route("/api/v1/<station>")
+def all_dates(station):
+    filename = "data\TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pandas.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data\TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pandas.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df["    DATE"].str.startswith(year)].to_dict(orient="records")
+    return result
+
+if __name__ == "__main__":
+    app.run(debug=True)
